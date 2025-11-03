cc359c67a8296074230664f908e0d10d0fd2353d
Mubashir Qureshi <mq901898@gmail.com>
2025-11-03 13:43:42 +0500

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,9 @@
+let name= prompt("What is Your name?");
+let age=prompt("What is your age?");
+let number=prompt("What is your Phone number");
+let qulification=prompt("What is your Phone Qulification?");
+document.getElementById("info").innerHTML=
+                                            "Your name is :"+""+ name+"</br>"+"</br>"+
+                                            "Your age is :"+""+ age+"</br>"+"</br>"+
+                                            "Your Phone is :"+""+number+"</br>"+"</br>"+
+                                            "Your Qulification is :"+""+qulification;
